24e298db259ba10af3d3affd6d6bfac60ce00ccb
Adria Montoto <75563346+adriiamontoto@users.noreply.github.com>
2024-05-17 11:32:54 +0200

    feat: improve translate prompt

--- backend/app/translate/functions/translate_text.py
+++ backend/app/translate/functions/translate_text.py
@@ -21,8 +21,18 @@ def translate_text(text: str, language: str) -> str:
     """
     client = ChatOpenAI(api_key=SecretStr(value=settings.OPENAI_API_KEY), model='gpt-3.5-turbo')
 
-    prompt_template = ChatPromptTemplate.from_template(
-        template='Translate the text that is delimited by triple backticks to {language}. text: ```{text}```')
+    prompt_template = ChatPromptTemplate.from_template(template="""
+        Translate the provided passage to {language}.
+
+        For example:
+        - If the passage is "I'm learning how to translate texts with LLM models." and the target language is "spanish",
+        the output should be "Estoy aprendiendo a traducir textos con modelos LLM.".
+        - If the passage is "Estic aprenent a traduir textos amb models LLM." and the target language is "english",
+        the output should be "I'm learning to translate texts with LLM models.".
+
+        Passage:
+        {text}
+    """)
 
     customer_messages = prompt_template.format_messages(language=language, text=text)
 
