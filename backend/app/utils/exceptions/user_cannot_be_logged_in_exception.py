3419270a919b29ead3dfcd6f4b158879dbf21024
Adria Montoto <75563346+adriiamontoto@users.noreply.github.com>
2024-05-18 21:39:11 +0200

    refactor: update UserCannotBeLoggedInException message to 'User already logged in.'

--- backend/app/utils/exceptions/user_cannot_be_logged_in_exception.py
+++ backend/app/utils/exceptions/user_cannot_be_logged_in_exception.py
@@ -8,12 +8,12 @@ class UserCannotBeLoggedInException(RuntimeError):
     Exception raised when the user cannot be logged in and is already logged in.
     """
 
-    def __init__(self, message: str = 'User cannot be logged in.') -> None:
+    def __init__(self, message: str = 'User already logged in.') -> None:
         """
         Initialize the exception with the message.
 
         Args:
-            message (str, optional): The message to be displayed. Defaults to 'User cannot be logged in'.
+            message (str, optional): The message to be displayed. Defaults to 'User already logged in.'.
         """
         self.message = message
         super().__init__(self.message)
