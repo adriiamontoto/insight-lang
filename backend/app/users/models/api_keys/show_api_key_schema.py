1c8fa4e6411e101e614377a8b7c1899ef112e5a6
Adria Montoto <75563346+adriiamontoto@users.noreply.github.com>
2024-05-18 18:55:36 +0200

    feat: add creation and last utilization date to return api key

--- backend/app/users/models/api_keys/show_api_key_schema.py
+++ backend/app/users/models/api_keys/show_api_key_schema.py
@@ -1,6 +1,8 @@
 """
 Schema for showing an API key.
 """
+from datetime import datetime, timedelta, timezone
+
 from pydantic import BaseModel, ConfigDict, Field
 from uuid import UUID
 
@@ -16,5 +18,13 @@ class ShowApiKey(BaseModel):
     secret_key: str = Field(default=...,
                             description='Secret key of the new API key.',
                             examples=['8873344efbff3fa9a8ca3dd0b742797b0018ce3cb1d6c23b0c424060f68f6e30'])
+
+    creation_date: datetime = Field(default=...,
+                                    description='Creation date of the API key.',
+                                    examples=[datetime.now(tz=timezone.utc)])
+
+    last_utilization_date: datetime | None = Field(default=None,
+                                                   description='Last utilization date of the API key.',
+                                                   examples=[datetime.now(tz=timezone.utc) + timedelta(hours=2)])
 
     model_config = ConfigDict(extra='ignore')
